585f7d1a551506fba80f2b05c1258dd528d3ba3b
noaione <noaione0809@gmail.com>
2020-10-25 20:56:33 +0700

    travis: add deployment and try to fix osx

--- tools/compile_travis.sh
+++ tools/compile_travis.sh
@@ -10,22 +10,28 @@ if [ "${TRAVIS_OS_NAME:-}" == "" ]; then
 	echo "Guessing OS"
 	if uname -s | grep -i Darwin &> /dev/null; then
 		TRAVIS_OS_NAME=osx
+        BUILD_FOR=OSX
 	elif uname -s | grep -i Linux &> /dev/null; then
 		TRAVIS_OS_NAME=linux
+        BUILD_FOR=Linux
 		if [ "${CCX:-}" == "" ]; then
 			# default to g++
-			export CCX=g++
+			export CXX=g++
 		fi
 	else
 		echo "Failed to guess OS"; exit 1
 	fi
 fi
 
+if [ "${BUILD_FOR:-}" == "" ]; then
+    BUILD_FOR=unknown
+fi
+
 echo "OS Detected: ${TRAVIS_OS_NAME}"
 ALL_MODULES=`ls src/modules/*.cpp`
-OUTPUT_FILE="OperCooked_${TRAVIS_OS_NAME}"
+OUTPUT_FILE="build/OperCooked_${BUILD_FOR}"
 
-echo "[${TRAVIS_OS_NAME}] Building..."
-echo "[${TRAVIS_OS_NAME}] Executing: ${CXX} src/main.cpp ${ALL_MODULES} -o ${OUTPUT_FILE} -O1 -s"
-$CXX src/main.cpp $ALL_MODULES -o $OUTPUT_FILE -O1 -s
-echo "[${TRAVIS_OS_NAME}] Artifact build!"
+echo "[${TRAVIS_OS_NAME}_${BUILD_FOR}] Building..."
+echo "[${TRAVIS_OS_NAME}_${BUILD_FOR}] Executing: ${CXX} src/main.cpp ${ALL_MODULES} -o ${OUTPUT_FILE} -O1 -s"
+$CXX src/main.cpp $ALL_MODULES -o $OUTPUT_FILE -O2 -s
+echo "[${TRAVIS_OS_NAME}_${BUILD_FOR}] Artifact build!"
