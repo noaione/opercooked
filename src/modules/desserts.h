ec11af93bfed9dd3e50325e0135bc411a5e347df
Gunawan272 <73239820+Gunawan272@users.noreply.github.com>
2020-11-15 20:49:23 +0700

    desserts: buat handler untuk membuat dessert baru (#4)

---
+++ src/modules/desserts.h
@@ -0,0 +1,16 @@
+#ifndef MOD_DESSERTS_H
+#define MOD_DESSERTS_H
+
+typedef struct Dessert{
+    int Extra_Time;
+    char Dessert_Name[200];
+    char Topping[25];
+    int Totaltime;
+    int Price; 
+    int modifier;
+    float callories;
+} Dessert;
+
+Dessert addDessert(void);
+
+#endif /* MOD_DESSERTS_H */
