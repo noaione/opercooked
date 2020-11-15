b3d0fc963a36779daf5c3cc1133aad62cdda7224
Andreas Alexander <73393181+Musarioth@users.noreply.github.com>
2020-11-15 23:56:24 +0700

    Update main.cpp
    update paling akhir (untuk sementara)

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,49 @@
 
 // Cukup satu int main
 // Jangan buat di file lain karena akan conflict.
-int main() {
-    puts("Selamat datang di Opercooked");
+//
+
+void Main_Menu(int profit){
+    puts("Welcome to Opercooked");
+   	printf("Today Profit: $%d\n", profit);
+    puts("==============================");
+    puts("1. Add Dessert or Beverages");
+    puts("2. View Cooking Process");
+    puts("3. View Order History");
+    puts("4. Order Dessert or Beverage");
+    puts("5. Exit");
+    printf(">> ");
+}
+
+int main(){
+    int profit = 0, option, flag = 1;
+    do{
+        do{
+            Main_Menu(profit);
+            scanf("%d", &option);
+	    } while(option > 5 || option < 1);
+
+        switch (option){
+        case 1:
+            Beverages minumanBaru = createBeverages();
+            Dessert dessertBaru = addDessert();
+            // printf("");
+            break;
+        
+        case 2:
+            Cooking_Progress();
+            break;
+        case 3:
+            viewOrderHistory();
+            break;
+        case 4:
+            Order_Menu();
+            break;
+        case 5:
+            printf("Thank You");
+            flag = 0;
+            return 0;
+        }
+    } while (flag == 1);
     return 0;
 }
