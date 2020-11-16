0c4841f4768fff0324b1e7f71e1ad05f6c833ab6
Randomizer23 <70153081+Randomizer23@users.noreply.github.com>
2020-11-16 19:32:26 +0700

    cookingProcess: buat handler utama cooking process (#9)

---
+++ src/modules/cookingProcess.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "cookingProcess.h"
+#include "beverages.h"
+#include "desserts.h"
+
+cookingData processing[1000];
+int ctr = 0;
+
+void cookBeverage(Beverages newDrink) {
+    processing[ctr].drinks = newDrink;
+    processing[ctr].price = newDrink.price;
+    processing[ctr].timer = newDrink.cooktime;
+    processing[ctr].type = 1;
+    ctr++;
+}
+
+void cookDessert(Dessert newDessert) {
+    processing[ctr].desserts = newDessert;
+    processing[ctr].price = newDessert.Price;
+    processing[ctr].timer = newDessert.Totaltime;
+    processing[ctr].type = 0;
+    ctr++;
+}
+
+cookedData viewCookProcess() {
+    if(ctr != 0) {
+        cookedData finished;
+        finished.totalCooked = -1;
+        //print header tabel
+        printf("| No\t| Type\t\t| Name\t\t\t\t| Price\t| Time Left\t|\n");
+		printf("-------------------------------------------------------------------------\n");
+
+        //print tabel
+        for(int i = 0; i < ctr; i++) {
+            if (processing[i].timer <= 0) {
+                finished.totalCooked++;
+                finished.cookedItems[finished.totalCooked] = processing[i];
+                ctr--;
+            } else {
+                if (processing[i].type == 1) {
+                    printf("| %d\t| Drink\t| %s\t\t\t\t| $%d\t| %d\t|\n", i + 1, processing[i].drinks.name, processing[i].price, processing[i].timer);
+                } else {
+                    printf("| %d\t| Dessert\t| %s\t\t\t\t| $%d\t| %d\t|\n", i + 1, processing[i].desserts.Dessert_Name, processing[i].price, processing[i].timer);
+                }
+                processing[i].timer -= 10;
+            }
+        }
+        puts("");
+        puts("Press Enter to return to main menu");
+        getchar();
+        return finished;
+    } else { //klo kosong
+        puts("There is no cooking process recently!");
+        puts("");
+        puts("Press Enter to continue");
+        getchar();
+
+        cookedData temp;
+        temp.totalCooked = -1;
+        return temp;
+    }
+}
---
+++ src/modules/cookingProcess.h
@@ -0,0 +1,24 @@
+#ifndef MOD_COOKINGPROCESS_H
+#define MOD_COOKINGPROCESS_H
+
+#include "beverages.h"
+#include "desserts.h"
+
+typedef struct cookingData {
+    Beverages drinks;
+    Dessert desserts;
+    int timer;
+    int price;
+    int type;
+} cookingData;
+
+typedef struct cookedData {
+    cookingData cookedItems[500];
+    int totalCooked;
+} cookedData;
+
+void cookBeverage(Beverages newDrink);
+void cookDessert(Dessert newDessert);
+cookedData viewCookProcess();
+
+#endif
